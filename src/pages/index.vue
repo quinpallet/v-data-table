746ea79644236bde30f75dd5d1f0137b6193778f
quinpallet <kurosaki4pers@gmail.com>
2024-08-27 23:47:58 +0900

    ヘッダーと複数列の固定

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,39 @@
 <script setup lang="ts">
+const headers = ref(headerList);
+const items = ref(itemList);
+// ヘッダーの各項目にwidthと同値のminWidthとmaxWidthを設定
+headers.value = headers.value.map((o) => ({
+  ...o,
+  minWidth: o.width,
+  maxWidth: o.width,
+}));
 </script>
 
 <template>
-    <v-btn text="Test"></v-btn>
+  <!-- <v-btn text="connect" @click="() => ble_connect()"></v-btn> -->
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    color="indigo"
+    theme="indigo"
+    items-per-page="100"
+    height="100vh"
+    density="compact"
+    fixed-header
+    sticky
+    hide-default-footer
+  ></v-data-table>
 </template>
+
+<style scoped lang="scss">
+.v-data-table {
+  :deep(table) {
+    thead {
+      th {
+        color: rgb(var(--v-theme-surface)) !important;
+        background: rgb(var(--v-theme-info)) !important;
+      }
+    }
+  }
+}
+</style>
